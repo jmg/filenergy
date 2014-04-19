24cf0a996c26fc262c96c2b7d9f43141e6cfcda1
Juan Manuel García <jmg.utn@gmail.com>
2014-04-19 18:09:03 -0300

    settings envs

--- filenergy/settings.py
+++ filenergy/settings.py
@@ -1,9 +1,18 @@
-secret_key = "\xa90\x91\xcd\xce\xf2\xbe\x1d\x87\xbb;\xa7\xf3\x91K\xde\x05*D\x9b6\xe4U\xbf"
+import os
 
-config = {
-    "SQLALCHEMY_DATABASE_URI": 'sqlite:////tmp/test.db',
+env = os.environ.get("ENV", "LOCAL")
+
+secret_key = "\xa90\x91\xcd\xce\xf2\xbe\x1d\x87\xbb;\xa7\xf3\x91K\xde\x05*D\x9b6\xe4U\xbf"
+login_view = '/user/login/'
+UPLOAD_DIR = "files"
+
+configs = {
+    "LOCAL": {
+        "SQLALCHEMY_DATABASE_URI": 'sqlite:////tmp/test.db',
+    },
+    "PROD": {
+        "SQLALCHEMY_DATABASE_URI": 'sqlite:////tmp/test.db',
+    }
 }
 
-login_view = '/user/login/'
-
-UPLOAD_DIR = "files"
+config = configs[env]
